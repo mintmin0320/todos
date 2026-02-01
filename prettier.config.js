590ab8538457bd8a64efa6ab7fe0888e07b926bf
mintmin0320 <mintmin0320@gmail.com>
2026-02-01 16:23:53 +0900

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  trailingComma: "all",
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  arrowParens: "always",
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+}
